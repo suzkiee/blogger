f5720b81266d99666e025ba3a51d3493d6bd49d7
Suzie K <suziekim.dev@gmail.com>
2021-06-07 11:16:56 -0400

    Add css stylesheets and complete I0

--- spec/features/articles/index_spec.rb
+++ spec/features/articles/index_spec.rb
@@ -2,8 +2,8 @@
 
 require "rails_helper"
 
-describe "user sees all articles" do
-  describe "they visit /articles" do
+RSpec.describe "Article index page" do
+  describe "user visits /articles" do
     before :each do 
       @article_1 = Article.create!(title: "Title 1", body: "Body 1")
       @article_2 = Article.create!(title: "Title 2", body: "Body 2")
