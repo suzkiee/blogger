0dd7dd7dbc60499e3ca45bd8051807a386466f4a
Suzie K <suziekim.dev@gmail.com>
2021-06-07 11:00:10 -0400

    Add links to view

--- spec/features/articles/index_spec.rb
+++ spec/features/articles/index_spec.rb
@@ -4,14 +4,21 @@ require "rails_helper"
 
 describe "user sees all articles" do
   describe "they visit /articles" do
-    it "displays all articles" do
-      article_1 = Article.create!(title: "Title 1", body: "Body 1")
-      article_2 = Article.create!(title: "Title 2", body: "Body 2")
-
+    before :each do 
+      @article_1 = Article.create!(title: "Title 1", body: "Body 1")
+      @article_2 = Article.create!(title: "Title 2", body: "Body 2")
       visit '/articles'
+    end 
+    
+    it "displays all articles" do
 
-      expect(page).to have_content(article_1.title)
-      expect(page).to have_content(article_2.title)
+      expect(page).to have_link(@article_1.title)
+      expect(page).to have_link(@article_2.title)
+    end
+
+    it "links to form to create new article" do
+
+      expect(page).to have_link("Create a New Article")
     end
   end
 end
