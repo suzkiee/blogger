5612cd7a7e19f2e1e4a922494e0d342fe81d090e
Suzie K <suziekim.dev@gmail.com>
2021-12-07 13:53:11 -0500

    [Refactor] ArticlesController\

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :destroy, :edit, :update]
+  
   def index
     @articles = Article.all
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def new
@@ -19,17 +20,15 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    Article.destroy(params[:id])
+    @article.destroy 
     
     redirect_to articles_path
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     @article.update(article_params)
 
     flash.notice = "Article was updated!"
@@ -41,4 +40,8 @@ class ArticlesController < ApplicationController
     def article_params 
       params.require(:article).permit(:title, :body)
     end
+
+    def set_article 
+      @article = Article.find(params[:id])
+    end 
 end
