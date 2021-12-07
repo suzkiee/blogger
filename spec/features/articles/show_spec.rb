9303b169081a7eb8694938bf3dc368eef8d8fbeb
Suzie K <suziekim.dev@gmail.com>
2021-12-07 15:15:53 -0500

    [Test] Red displays the comment on the article show page

--- spec/features/articles/show_spec.rb
+++ spec/features/articles/show_spec.rb
@@ -23,6 +23,23 @@ RSpec.describe "Article show page" do
           expect(page).to have_content(@comment_2.body)
         end
       end
+
+      describe "they fill in a comment form" do 
+        it "displays the comment on the article show" do 
+          article = Article.create!(title: "New Title", body: "New Body")
+
+          visit article_path(article)
+
+          fill_in "comment[author_name]", with: "ME!"
+          fill_in "comment[body]", with: "So many thoughts on this article"
+          click_on "Submit"
+
+          expect(current_path).to eq(article_path(article))
+          expect(page).to have_content("Post a Comment")
+          expect(page).to have_content("ME!")
+          expect(page).to have_content("So many thoughts on this article.") 
+        end
+      end
     end
   end
 end
